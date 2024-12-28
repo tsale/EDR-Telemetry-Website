ffbf3c045cf4d51fa6755c77c5ac414bc1ba3d54
Kostas <kostastsale@gmail.com>
2024-12-28 13:32:13 -0800

    Add new contributor and enhance social link interactions

--- js/contributors.js
+++ js/contributors.js
@@ -141,8 +141,14 @@ class ContributorsManager {
                 handle: "cybersecdiva", 
                 linkedin: "tanisha-l-t-079a7b17",
                 twitter: "cybersecdiva",
-                tier: "basic" // Added tier information
+                tier: "basic"
             },
+            {
+                name: "Daniel Koifman",
+                linkedin: "daniel-koifman-61072218b",
+                customImage: "https://media.licdn.com/dms/image/v2/D4D03AQFubXcH8Y2myw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1693372981400?e=1740614400&v=beta&t=ROKPDzdKaEH2nDpYNJuZhrxMm6yF2ou2M1S1LPySwbg",
+                tier: "basic"
+            }
         ];
 
         const tierBadges = {
@@ -155,21 +161,22 @@ class ContributorsManager {
             <div class="subscribers-grid">
                 ${subscribers.map(subscriber => `
                     <div class="subscriber-item ${subscriber.tier}">
-                        <img src="https://unavatar.io/twitter/${subscriber.handle}" 
+                        <img src="${subscriber.twitter ? `https://unavatar.io/twitter/${subscriber.handle}` : (subscriber.customImage || './images/edr_telemetry_logo.png')}" 
                              alt="@${subscriber.handle}" 
                              class="subscriber-avatar"
                              onerror="this.src='./images/edr_telemetry_logo.png'" />
                         <div class="subscriber-info">
                             <div class="subscriber-tier">${tierBadges[subscriber.tier]}</div>
                             <div class="subscriber-name" title="${subscriber.name}">${subscriber.name}</div>
-                            <div class="subscriber-handle">@${subscriber.handle}</div>
-                            <div class="subscriber-social">
+                            ${subscriber.handle ? `<div class="subscriber-handle">@${subscriber.handle}</div>` : ''}
+                            <div class="subscriber-social" style="position: relative; z-index: 2;">
                                 ${subscriber.twitter ? `
                                     <a href="https://twitter.com/${subscriber.twitter}" 
                                        target="_blank" 
                                        rel="noopener noreferrer" 
                                        class="social-link twitter" 
-                                       title="Follow on Twitter">
+                                       title="Follow on Twitter"
+                                       onclick="event.stopPropagation();">
                                         <svg class="social-icon" viewBox="0 0 16 16" fill="currentColor" width="14" height="14">
                                             <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334q.002-.211-.006-.422A6.7 6.7 0 0 0 16 3.542a6.7 6.7 0 0 1-1.889.518 3.3 3.3 0 0 0 1.447-1.817 6.5 6.5 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.32 9.32 0 0 1-6.767-3.429 3.29 3.29 0 0 0 1.018 4.382A3.3 3.3 0 0 1 .64 6.575v.045a3.29 3.29 0 0 0 2.632 3.218 3.2 3.2 0 0 1-.865.115 3 3 0 0 1-.614-.057 3.28 3.28 0 0 0 3.067 2.277A6.6 6.6 0 0 1 .78 13.58a6 6 0 0 1-.78-.045A9.34 9.34 0 0 0 5.026 15"/>
                                         </svg>
@@ -180,7 +187,8 @@ class ContributorsManager {
                                        target="_blank" 
                                        rel="noopener noreferrer" 
                                        class="social-link linkedin" 
-                                       title="Connect on LinkedIn">
+                                       title="Connect on LinkedIn"
+                                       onclick="event.stopPropagation();">
                                         <svg class="social-icon" viewBox="0 0 16 16" fill="currentColor" width="14" height="14">
                                             <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
                                         </svg>
